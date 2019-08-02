b0cac83c41d58e3d9478114540a1f3deb956735a
rawel2 <rawel2@orange.pl>
2019-08-02 14:40:09 +0200

    ok

--- colorduino_firmware/colorduino_firmware.ino
+++ colorduino_firmware/colorduino_firmware.ino
@@ -23,6 +23,9 @@
 #define START_OF_DATA 0x10       //data markers
 #define END_OF_DATA 0x20         //data markers
 
+
+void receiveEvent(int numBytes);
+
 void setup()
 {
   Colorduino.Init();
@@ -121,10 +124,3 @@ void loop()
 void receiveEvent(int numBytes) {
   //do nothing here
 }
-
-
-
-
-
-
-
